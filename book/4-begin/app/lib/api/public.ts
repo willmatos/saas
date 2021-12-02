db39bc547298b291e30bd3b83e3d9ac00b1ed1a9
William Matos <will.matos@gmail.com>
2021-12-02 12:59:32 -0600

    Remove Book Directory

--- book/4-begin/app/lib/api/public.ts
+++
@@ -1,9 +0,0 @@
-import sendRequestAndGetResponse from './sendRequestAndGetResponse';
-
-const BASE_PATH = '/api/v1/public';
-
-export const getUserApiMethod = (request) =>
-  sendRequestAndGetResponse(`${BASE_PATH}/get-user`, {
-    request,
-    method: 'GET',
-  });
